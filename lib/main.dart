cd2c118d5bed1587f3844f37afdc1ba3954aa9d5
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-06-18 19:04:30 +0530

    container features done

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,41 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("hi flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          child: Text(
+            "I am a box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [
+              Colors.blue,
+              Colors.green,
+            ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-// stateless widget does not change its state
+// stateless widget is something that is not suppose to change.
+//container is a type of box
+//it is a shade which is made of combition of two or more colors.
