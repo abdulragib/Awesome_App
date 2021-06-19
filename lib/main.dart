d9061767481d3c3ac07137a695e511be7846a52f
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-06-19 12:51:39 +0530

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,42 +14,90 @@ void main() {
   );
 }
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var myText = "Change my name";
+  TextEditingController _nameController =
+      TextEditingController(); // the value of the field
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        color: Colors.teal,
-        height: 100,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/christopher-gower-m_HRfLhgABo-unsplash.jpg",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(padding: const EdgeInsets.all(16.0)),
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      hintText: "Enter some Text",
+                      labelText: "Name",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
+            UserAccountsDrawerHeader(
+              accountName: Text("Abdul Ragib"),
+              accountEmail: Text("abdulragib36@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://instagram.fpat2-2.fna.fbcdn.net/v/t51.2885-19/s320x320/202275436_784096098917607_8868512262180276103_n.jpg?tp=1&_nc_ht=instagram.fpat2-2.fna.fbcdn.net&_nc_ohc=xO86ZQw4b20AX-GZNGF&edm=ABfd0MgBAAAA&ccb=7-4&oh=b42b5467f97eaf0c302d6e9de7d8edf6&oe=60D4CE54&_nc_sid=7bff83"),
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Abdul Ragib"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.mail),
+              title: Text("Email"),
+              subtitle: Text("abdulragib36@gmail.com"),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
@@ -63,3 +111,8 @@ class Homepage extends StatelessWidget {
 
 //mainAxisAlignment: MainAxisAlignment.spaceBetween is used to give space between container.
 //mainAxisAlignment: MainAxisAlignment.spaceevenly is used to give equal space.
+
+//floatingActionButtonLocation: FloatingActionButtonLocation.endFloat used to locate the floating button,
+
+//State<Homepage> used to give current state of screen. ex- what is happening on the screen
+//if u are a human what is ur state like wheather ur sitting,walking,doing something is a state. everthing on ui is a state.
